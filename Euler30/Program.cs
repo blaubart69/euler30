97b00085628b3b683228ee162487b66e1f7e6f7b
bee <bernhard.spindler.75@gmail.com>
2018-11-22 18:03:37 +0100

    added pow5 cache

--- Euler30/Program.cs
+++ Euler30/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Euler30
 {
@@ -15,31 +11,37 @@ namespace Euler30
 
         static void Main(string[] args)
         {
+            ulong[] POW5 = new ulong[10];
+            for (ulong i=0; i<10;++i)
+            {
+                POW5[i] = pow5(i);
+            }
+
             ulong sum = 0;
             ulong currNum = 0;
             for (uint a=0; a < 10; ++a)
             {
-                ulong powA = pow5(a);
+                ulong powA = POW5[a];
                 for (uint b = 0; b < 10; ++b)
                 {
-                    ulong SumAB = powA + pow5(b);
+                    ulong SumAB = powA + POW5[b];
                     for (uint c = 0; c < 10; ++c)
                     {
-                        ulong sumABC = SumAB + pow5(c);
+                        ulong sumABC = SumAB + POW5[c];
                         for (uint d = 0; d < 10; ++d)
                         {
-                            ulong SumABCD = sumABC + pow5(d);
+                            ulong SumABCD = sumABC + POW5[d];
                             for (uint e = 0; e < 10; ++e)
                             {
-                                ulong SumABCDE = SumABCD + pow5(e);
+                                ulong SumABCDE = SumABCD + POW5[e];
                                 for (uint f = 0; f < 10; ++f)
                                 {
-                                    ulong SumABCDEF = SumABCDE + pow5(f);
+                                    ulong SumABCDEF = SumABCDE + POW5[f];
                                     for (uint g = 0; g < 10; ++g)
                                     {
                                         ulong powered = SumABCDEF + pow5(g);
 
-                                        if (currNum > 1 && currNum == powered)
+                                        if (currNum == powered)
                                         {
                                             Console.Out.WriteLine($"currNum {currNum} = powered {a}+{b}+{c}+{d}+{e}+{f}+{g}");
                                             sum += powered;
@@ -53,7 +55,8 @@ namespace Euler30
                     }
                 }
             }
-            Console.WriteLine(sum);
+            sum -= 1;
+            Console.WriteLine($"sum-1 = {sum}");
         }
 
         private static ulong calcNum(uint a, uint b, uint c, uint d, uint e, uint f, uint g)
